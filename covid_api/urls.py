1ee45f84ce08be747f241aededd380637d8dad17
alavarello <alavarello@itba.edu.ar>
2020-07-08 19:46:53 -0300

    ✨ ADD: Add home redirect to swagger docs

--- covid_api/urls.py
+++ covid_api/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import RedirectView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -19,10 +19,13 @@ schema_view = get_schema_view(
 )
 
 
+api_version = 'api/v1'
+
 urlpatterns = [
-    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^api/swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    url(r'^api/redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    url(r'^api/v1/', include('covid_api.core.urls')),
+    re_path(f'^{api_version}/swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(f'^{api_version}/redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(f'^{api_version}/', include('covid_api.core.urls')),
     path('admin/', admin.site.urls),
+    re_path('^', RedirectView.as_view(pattern_name='schema-swagger-ui', permanent=False)),
 ]
+
