2b3a72d5e2932282cc1bc51cc37d2dd2d7160c73
Unknown <jpalacci@itba.edu.ar>
2020-07-09 12:38:43 -0300

    Change base URL

--- covid_api/urls.py
+++ covid_api/urls.py
@@ -16,7 +16,7 @@ schema_view = get_schema_view(
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
-   url="https://covid19api.it.itba.edu.ar/api"
+   url="https://covid-itba-api.herokuapp.com/api"
 )
 
 
